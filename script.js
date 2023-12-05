4f9016a0c775fc7a739b54b1eb89759ae49a6faa
Jackson Pipe <jackbpipe@gmail.com>
2023-12-04 19:45:15 -0500

    Update project section and add GitHub repositories

--- script.js
+++ script.js
@@ -8,4 +8,26 @@ document.addEventListener('DOMContentLoaded', function() {
         nav.classList.toggle('nav-visible'); // Toggle the class
         console.log('nav-toggle clicked');
     });
+
+    
+    fetch('https://api.github.com/users/Waxter88/repos')
+    .then(response => response.json())
+    .then(data => {
+        const reposContainer = document.getElementById('projects-section');
+        data.forEach(repo => {
+            const repoElement = document.createElement('a');
+            repoElement.className = 'project';
+            repoElement.href = repo.html_url;
+            repoElement.target = '_blank';
+            repoElement.innerHTML = `
+                <h3>${repo.name}</h3>
+                <p>${repo.description || 'No description available'}</p>
+            `;
+            reposContainer.appendChild(repoElement);
+        });
+    })
+    .catch(error => console.log('Error fetching GitHub repositories:', error));
+
+    
+    
 });
