3f6e987409c2bf24b454602249d50f45e7164df4
Waxter88 <111841440+Waxter88@users.noreply.github.com>
2023-12-06 21:22:51 +0000

    Add dark mode toggle button

--- script.js
+++ script.js
@@ -28,6 +28,21 @@ document.addEventListener('DOMContentLoaded', function() {
     })
     .catch(error => console.log('Error fetching GitHub repositories:', error));
 
-    
+    document.getElementById('theme-toggle').addEventListener('click', function() {
+        // Determine the current theme
+        var currentTheme = document.documentElement.getAttribute('data-theme');
+      
+        // Switch between 'light' and 'dark'
+        if (currentTheme === 'dark') {
+          document.documentElement.setAttribute('data-theme', 'light');
+          // Update the href attribute to point to the light theme stylesheet
+          document.getElementById('theme-style').href = 'light-theme.css';
+        } else {
+          document.documentElement.setAttribute('data-theme', 'dark');
+          // Update the href attribute to point to the dark theme stylesheet
+          document.getElementById('theme-style').href = 'styles.css';
+        }
+      });
+      
     
 });
